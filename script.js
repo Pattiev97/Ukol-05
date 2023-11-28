b5ef847278278aca4a0c99881d1add3f87ddc9c5
Patricie Vyhlídalová <patina.vy@gmail.com>
2023-11-28 21:24:48 +0100

    Výpočet mzdy

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Vytvořte funkci salary se třemi parametry:
+- wage - hrubá mzda v korunách za hodinu
+- hours - kolik hodin denně průměrně pracujete
+- days - kolik dní v měsící průměrně pracujete
+Funkce spočítá vaši hrubou měsíční mzdu v celých korunách.
+
+Dále vytvořte funkci taxed, která na vstupu obdrží částku a procento zdanění, a vrátí částku v celých korunách zdaněnou podle zadaných procent.
+
+Použítím funkcí salary a taxed spočítejte svoji měsíční mzdu po 15% zdanění.*/
+
+const salary = (wage, hours, days) => {
+    const hrubaMzda = wage * hours * days
+    return Math.round(hrubaMzda)
+}
+
+const taxed = (castka, procentoZdaneni) => {
+    const dan = (castka * procentoZdaneni) / 100
+    return Math.round(dan)
+}
+
+const mesicniHrubaMzda = salary(180, 8, 21)
+const vypocitanaDan = taxed(mesicniHrubaMzda, 15)
+const zdanenaCastka = mesicniHrubaMzda - vypocitanaDan
+
+document.body.innerHTML = `Měsíční mzda po zdanění dělá ${zdanenaCastka} Kč.`
